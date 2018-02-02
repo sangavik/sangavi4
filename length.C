709e1f3770850c4f3100dd6775a3e8501f3968de
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-02 16:36:21 +0530

    length.C

---
+++ length.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(0
+{
+char text[200];
+int length;
+printf("\n enter the string");
+scanf("%s",&text);
+len=str len(text);
+printf("\n the length of string is :",&len);
+}
