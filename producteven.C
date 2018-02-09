4a55b4298a35419daf7ed57f9ebd7829bb93b9ab
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-09 16:54:50 +0530

    producteven.C

---
+++ producteven.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int m,n,c;
+printf("\n enter the values of a and b");
+scanf("%d %d",&m,&n);
+c=m*n;
+if(c%2==0)
+{
+printf("yes\n");
+}
+else
+{
+printf("no\n");
+}
+return 0;
+}
